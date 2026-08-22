7128ae1989a8bb92fb02e7c25f793d766076e758
OneSeedSocial <ea.ilustraciones@gmail.com>
2026-08-22 12:37:54 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.ONESEED_CONFIG = {
-  supabaseUrl: 'https://dreinrfakkkvhvtbnczs.supabase.co',
-  supabasePublishableKey: 'sb_publishable_NEYCRz-tfxebhx8onTwyjA_AzMQQrzY',
-  waitlistEnabled: true,
-};
